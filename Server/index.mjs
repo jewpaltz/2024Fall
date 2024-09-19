fbc50551ca80cdf4465859f0f555eaf8b51ff615
Moshe Plotkin <plotkinm@newpaltz.edu>
2024-09-19 15:16:31 -0400

    started node

---
+++ Server/index.mjs
@@ -0,0 +1,16 @@
+// server.mjs
+import { createServer } from 'node:http';
+
+const PORT = 3000;
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(PORT, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
