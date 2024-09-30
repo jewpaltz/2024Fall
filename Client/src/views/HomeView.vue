7ad30d263fcde33766b65213421945557cc46ca1
Moshe Plotkin <plotkinm@newpaltz.edu>
2024-09-30 14:56:23 -0400

    Got rid of boilerplate

--- Client/src/views/HomeView.vue
+++ Client/src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
+    <p>Welcome to your Vue.js app!</p>
   </main>
 </template>
