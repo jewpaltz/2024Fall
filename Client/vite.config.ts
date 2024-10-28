df84fbe46b9c8adc0aa6916aece023ab214c5064
Moshe Plotkin <plotkinm@newpaltz.edu>
2024-10-28 11:11:27 -0400

    vite build

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,24 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    VueRouter({
-      /* options */
-    }),
-    vue(),
-    vueJsx(),
-    vueDevTools()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [
+      VueRouter({
+        /* options */
+      }),
+      vue(),
+      vueJsx(),
+      vueDevTools()
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    base: env.BASE_URL
   }
 })
