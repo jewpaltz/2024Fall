4f1806143d8b56261ced9ea857fa37d88c5ecf08
Moshe Plotkin <plotkinm@newpaltz.edu>
2024-11-01 07:09:00 -0400

    Very beginning of Express and Tooling for REST development

---
+++ Server/index.js
@@ -0,0 +1,14 @@
+/*  B"H
+ */
+const express = require("express")
+const app = express()
+
+const PORT = 3000
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running at http://localhost:" + PORT)
+})
